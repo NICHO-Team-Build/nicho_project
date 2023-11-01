997b0e3321366d572a5f1ff3ee537a6b6a4c9a70
Zqy0319 <pl04030319@gmail.com>
2023-11-01 17:58:35 +1100

    improve border of the picture and add download as pdf function

--- src/components/MineInformation.jsx
+++ src/components/MineInformation.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import "../styles/mineInformation.css";
 
 import NavBar from "./NavBar";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import MineInformation1 from "../assets/MineInformation1.png";
 import MineInformation2 from "../assets/MineInformation2.png";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const MineInformation = () => {
     const [mine, setMine] = useState({
@@ -15,12 +18,35 @@ const MineInformation = () => {
         mineScale: "Medium-Sized Mine",
     });
 
+    
     // useEffect(() => {
     //     get data from backend and use setMine
     // })
+    
+    //export to a pdf
+    function printDocument() {
+        const input = document.getElementById("container");
+        html2canvas(input).then((canvas) => {
+            let imgWidth = 208;
+            const pageHeight = 295;
+            const imgHeight = (canvas.height * imgWidth) / canvas.width;
+            let heightLeft = imgHeight;
+            let position = 0;
+            heightLeft -= pageHeight;
+            const doc = new jsPDF("p", "mm");
+            doc.addImage(canvas, 'PNG', 0, position, imgWidth, imgHeight, '', 'FAST');
+            while (heightLeft > 0) {
+                position = heightLeft - imgHeight;
+                doc.addPage();
+                doc.addImage(canvas, 'PNG', 0, position, imgWidth, imgHeight, '', 'FAST');
+                heightLeft -= pageHeight;
+            }
+            doc.save(mine.name + '.pdf');
+        });
+    }
 
     return (
-        <div className="container">
+        <div id="container">
             <div className="header">
                 <span className="title">Opal Oasis</span>
                 <span className="data-selector">DATA #01 SELECT BY LEFT</span>
@@ -29,23 +55,29 @@ const MineInformation = () => {
                 <NavBar />
             </div>
             <div className="model-section">
-                <img
-                    src={MineInformation1}
-                    alt="Crystal"
+                <div
                     className="crystal-image"
-                />
-                <div>
-                    <div className="name">{mine.name}</div>
-                </div>
-                <div>
-                    <div className="location">{mine.location}</div>
-                </div>
-                <div>
-                    <div className="facility">{mine.facility}</div>
+                    style={{ backgroundImage: `url(${MineInformation1})` }}
+                >
+                    <div className="model-info">
+                        <div className="mine-name">
+                            <div>{mine.name}</div>
+                        </div>
+                        <div className="mine-location">
+                            
+                            <div><LocationOnIcon /> {mine.location}</div>
+                        </div>
+                        <div className="mine-facility">
+                            <div>{mine.facility}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="info-section">
-                <img src={MineInformation2} alt="Mine" className="mine-image" />
+                <div
+                    className="mine-image"
+                    style={{ backgroundImage: `url(${MineInformation2})` }}
+                ></div>
                 <div className="carat">
                     <div className="info-title">PER CARAT</div>
                     <div>${mine.perCarat}</div>
@@ -64,7 +96,7 @@ const MineInformation = () => {
             </div>
             <div className="footer">
                 <span className="info">INFO</span>
-                <span className="preview-download">PREVIEW / DOWNLOAD</span>
+                <button className="download" onClick={printDocument}>DOWNLOAD</button>
             </div>
         </div>
     );
